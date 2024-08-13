d583dc766859aa094f4f6d86573d6919f0bc07dd
Scott Peshak <scott@peshak.net>
2024-08-12 19:05:59 -0500

    Improve entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,17 @@ INPUT_DIR=/input
 OUTPUT_DIR=/output
 
 while true; do
-    for INPUT_FILE in "$INPUT_DIR"/*.pdf
-    do
-      if [ -f "$INPUT_FILE" ]; then
-        scanprep "$INPUT_FILE" "$OUTPUT_DIR"
-        rm "$INPUT_FILE"
-      fi
-    done
+  ls -l "$INPUT_DIR"
 
-  sleep 120
+  for INPUT_FILE in "$INPUT_DIR"/*.pdf
+  do
+    if [ -f "$INPUT_FILE" ]; then
+      scanprep "$INPUT_FILE" "$OUTPUT_DIR"
+      rm "$INPUT_FILE"
+    fi
+  done
+
+  ls -l "$OUTPUT_DIR"
+
+  sleep ${LOOP_DELAY:-120}
 done
